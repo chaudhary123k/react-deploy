528fe5642a3de8fc37f13321c6d35f4d4c3326de
chaudhary123k <108264222+chaudhary123k@users.noreply.github.com>
2022-06-28 11:10:50 +0530

    deploy my app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{backgroundColor : "orange"}}>
+     <h1 style={{color: "red"}}>My First app</h1>
+     <p></p>
+    </div>
+  );
+}
+
+export default App;
